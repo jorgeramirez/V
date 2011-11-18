c3f5ef1ef01e78e3f87cfe3c2e059746124698a9
Jorge Ramirez <jorgeramirez1990@gmail.com>
2011-11-18 13:27:13 -0300

    Control de Enabled/Disabled de Boton Delete.

--- v_gwtapp/v_gwtapp/src/v/client/widgets/CrudGrid.java
+++ v_gwtapp/v_gwtapp/src/v/client/widgets/CrudGrid.java
@@ -8,12 +8,15 @@ import v.client.AppConstants.Filtros;
 import com.extjs.gxt.ui.client.data.BeanModel;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.extjs.gxt.ui.client.data.RpcProxy;
+import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
+import com.extjs.gxt.ui.client.event.SelectionChangedListener;
 import com.extjs.gxt.ui.client.store.Store;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.grid.CheckBoxSelectionModel;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+import com.google.gwt.user.client.Element;
 
 /**
  * Clase que Define un {@link Grid} con capacidad de realizar CRUD sobre
@@ -23,6 +26,8 @@ import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
  **/
 
 public class CrudGrid<M> extends CustomGrid<M> {
+	private Button addButton;
+	private Button deleteButton;
 
 	public CrudGrid(String title, ColumnModel cm, HashMap<String, AppConstants.Filtros> filtersConfig, 
 			  		RpcProxy<PagingLoadResult<M>> proxy) {
@@ -42,13 +47,43 @@ public class CrudGrid<M> extends CustomGrid<M> {
 	@Override
 	protected ToolBar createTopToolBar() {
 		ToolBar tb = super.createTopToolBar();
-		Button addButton = new Button("Agregar");
+		addButton = new Button("Agregar");
 		addButton.setIconStyle("icon-add");
 		tb.add(addButton);
-		Button delButton = new Button("Eliminar");
-		delButton.setIconStyle("icon-delete");
-		tb.add(delButton);
+		deleteButton = new Button("Eliminar");
+		deleteButton.setIconStyle("icon-delete");
+		deleteButton.setEnabled(false);
+		tb.add(deleteButton);
 		return tb;
-	}	
+	}
+	
+	public void onRender(Element target, int index) {
+		super.onRender(target, index);
+		
+		//Controlamos enabled/disabled del botón delete del ToolBar
+		this.getGrid().getSelectionModel().addSelectionChangedListener(new SelectionChangedListener<BeanModel>() {
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent<BeanModel> se) {
+				deleteButton.setEnabled(se.getSelection().size() > 0);
+			}
+		});
+	}
+
+	public Button getAddButton() {
+		return addButton;
+	}
+
+	public void setAddButton(Button addButton) {
+		this.addButton = addButton;
+	}
+
+	public Button getDeleteButton() {
+		return deleteButton;
+	}
+
+	public void setDeleteButton(Button deleteButton) {
+		this.deleteButton = deleteButton;
+	}
 	
 }
